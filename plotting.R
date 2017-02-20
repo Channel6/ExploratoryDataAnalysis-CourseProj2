22ae250e3b47341e32a42f4ae52cdee7ecb70c5a
Glenn Kerbein <glenn@spontaneousdancing.net>
2017-02-20 13:05:08 -0800

    finish plot 4

--- plotting.R
+++ plotting.R
@@ -102,4 +102,39 @@ qplot(
 )
 
 ## plot 4
+SCC.coal <- grep("coal", SCC$Short.Name, ignore.case = TRUE)
+SCC.coal <- SCC[SCC.coal, ]
+SCC.identifiers <- as.character(SCC.coal$SCC)
+
+NEI$SCC <- as.character(NEI$SCC)
+NEI.coal <- NEI[NEI$SCC %in% SCC.identifiers, ]
+
+aggregate.coal <- with(
+	NEI.coal,
+	aggregate(
+		Emissions,
+		by = list(year),
+		sum
+	)
+)
+colnames(aggregate.coal) <- c("year", "Emissions")
+
+png(
+	filename="plot4.png",
+	width=504,
+	height=504,
+	units="px"
+)
+
+plot(
+	aggregate.coal,
+	type = "o",
+	ylab = expression("Total Emissions, PM"[2.5]), 
+	xlab = "Year",
+	main = "Emissions and Total Coal Combustion for the United States", 
+	xlim = c(1999, 2008)
+)
+
+polygon(aggregate.coal, col = "red", border = "red")
+
 
