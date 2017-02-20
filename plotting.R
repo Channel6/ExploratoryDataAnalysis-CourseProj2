2cd300c23ead9db2e9f53387596e655d7866aec4
Glenn Kerbein <glenn@spontaneousdancing.net>
2017-02-20 13:07:05 -0800

    add plot 5

--- plotting.R
+++ plotting.R
@@ -137,4 +137,38 @@ plot(
 
 polygon(aggregate.coal, col = "red", border = "red")
 
+## plot 5
+SCC.motor <- grep("motor", SCC$Short.Name, ignore.case = TRUE)
+SCC.motor <- SCC[SCC.motor, ]
+SCC.identifiers <- as.character(SCC.motor$SCC)
+NEI$SCC <- as.character(NEI$SCC)
+NEI.motor <- NEI[NEI$SCC %in% SCC.identifiers, ]
+
+NEI.motor.24510 <- NEI.motor[which(NEI.motor$fips == "24510"), ]
+
+aggregate.motor.24510 <- with(
+	NEI.motor.24510,
+	aggregate(
+		Emissions,
+		by = list(year), 
+		sum
+	)
+)
+
+png(
+	filename="plot5.png",
+	width=504,
+	height=504,
+	units="px"
+)
+
+plot(
+	aggregate.motor.24510,
+	type = "o",
+	ylab = expression("Total Emissions, PM"[2.5]),
+	xlab = "Year",
+	main = "Total Emissions from Motor Vehicle Sources"
+)
+
+## plot 6
 
