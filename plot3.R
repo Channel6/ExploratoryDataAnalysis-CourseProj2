754f133e0bfcd9646ffa19a02b7dd775da56c7a6
Glenn Kerbein <glenn@spontaneousdancing.net>
2017-02-20 13:19:36 -0800

    split out plot 3

---
+++ plot3.R
@@ -0,0 +1,51 @@
+#### Exploratory Data Analysis -- Course Project 2 ###
+##			Glenn Kerbein		    ##
+##		      February 20, 2017		    ##
+##		           Plot 2		    ##
+######################################################
+
+## intiailaize data requirements
+## put all plots in this main plotting.R file, and output the
+### individual plots as needed
+library(plyr)
+library(ggplot2)
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+BatltimoreMD <- NEI[which(NEI$fips == "24510"), ]
+aggregate.24510 <- with(
+	BatltimoreMD,
+	aggregate(
+		Emissions,
+		by = list(year),
+		sum
+	)
+)
+colnames(aggregate.24510) <- c("year", "Emissions")
+
+BatltimoreMD.type <- ddply(
+	BatltimoreMD,
+	.(type, year),
+	summarize,
+	Emissions = sum(Emissions)
+)
+BatltimoreMD.type$Pollutant_Type <- BatltimoreMD.type$type
+
+png(
+	filename="plot3.png",
+	width=504,
+	height=504,
+	units="px"
+)
+
+qplot(
+	year,
+	Emissions,
+	data = BatltimoreMD.type,
+	group = Pollutant_Type,
+	color = Pollutant_Type,
+	geom = c("point", "line"),
+	ylab = expression("Total Emissions, PM"[2.5]), 
+	xlab = "Year",
+	main = "Total Emissions in U.S. by Type of Pollutant"
+)
